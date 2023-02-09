b56bf2cc4ee87f8aed8522e7a7031289e33ebc99
vijaykumar <vijaykumarpichuka>
2023-02-09 08:21:20 +0530

    Important Q_A

---
+++ important_Q_A.py
@@ -0,0 +1,18 @@
+# Shallow and DeepCopy in Python
+# ShallowCopy: A shallow copy constructs a new compound object and then (to the extent possible) inserts references into it to the objects found in the original.
+# Deep Copy: A deep copy constructs a new compound object and then, recursively, inserts copies into it of the objects found in the original.
+
+import copy
+l = [[1,2], [3,4], [5,6], [7,8]]
+# Shallow And Deep Copy
+# ShallowCopy:
+shallow = l.copy()
+deep = copy.deepcopy(l)
+print('Original list ',l)
+print('Before Shallow Copy ',shallow)
+print('Before Shallow Copy ',deep)
+
+l[0][0] = 20
+print('After list ',l)
+print('After ShallowCopy ',shallow)
+print('After DeepCopy ',deep)
